a1d31b584e13a8daf0338ea7628d11c969808db9
yuanwang <yuanwang33@foxmail.com>
2022-02-02 01:52:12 +0800

    init

---
+++ src/main/java/com/eltonyuen/demo/wiresharkdemo/WiresharkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.eltonyuen.demo.wiresharkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiresharkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiresharkDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eltonyuen/demo/wiresharkdemo/WiresharkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eltonyuen.demo.wiresharkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WiresharkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
